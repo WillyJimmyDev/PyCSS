641d8776234d64e7b5443d0a2b0e94af416fd3ef
ElanMan <ben@elanman.com>
2013-09-02 20:20:32 +0100

    Added parsing for css ids in html

--- PyCSS/input.py
+++ PyCSS/input.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-import os
 import glob
 import re
 
-holder = []
+classholder = []
+idholder = []
 
 userdir = input("Enter a directory to search\n")
 userexts = input("Enter a comma separated list of file extensions to search in e.g php,html\n")
@@ -16,27 +16,43 @@ for ext in exts:
     print ("current file extension is ", ext)
 
     files = glob.glob(userdir + "*." + ext)
-
+    print("number of files searched", len(files))
     for file in files:
         
         print("current file name is ", file)
         handle = open(file, "r")
         content = handle.readlines()
         linenum = 0
+        classfound = False
+        idfound = False
         
         for line in content:
 
             classes = re.findall(r'class=\"(.+?)\"', line)
             if classes:
+                classfound = True
                 for cssclass in classes:
-                    print(" class found: ", cssclass, "on line ", linenum)
-                    holder.append(cssclass)
+                    print(" class found -> .", cssclass, " on line ", linenum, sep='')
+                    classholder.append(cssclass)
             
+            ids = re.findall(r'id=\"(.+?)\"', line)
+            if ids:
+                idfound = True
+                for cssid in ids:
+                    print(" id found -> #", cssid, " on line ", linenum,sep='')
+                    idholder.append(cssid)
+                    
             linenum = linenum + 1
+            
+        if not classfound:
+            print("no classes found in file")
+            
+        if not idfound:
+            print("no ids found in file")
 
 # holder now holds all classes mentioned in the files
-print(holder)
+print("class list is ", classholder)
+print("id list is ", idholder)
 
 #need multi-demensional array to hold filename, line number of occurrence ,class name, id name etc
-#need a separate loop for ids
 #need a list of all css files linked to from file
