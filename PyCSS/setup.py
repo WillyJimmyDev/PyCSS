2d94a57c80ec7c3cb9c06cb24ca9904699e89e93
ElanMan <ben@elanman.com>
2016-07-13 13:04:55 +0100

    Restructured project files

---
+++ PyCSS/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyCSS',
+    version='',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='elanman',
+    author_email='ben@elanman.com',
+    description=''
+)
